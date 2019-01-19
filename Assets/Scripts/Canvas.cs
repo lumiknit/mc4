55031a9017f8fe02895c3bab83475f522bfb6a98
lumiknit <aasr4r4@gmail.com>
2019-01-19 13:22:59 +0900

    First Commit

---
+++ Assets/Scripts/Canvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas : MonoBehaviour
+{
+    public GameObject toast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    public void MakeToast(string message, float duration) {
+        GameObject t = Instantiate(toast);
+        t.transform.SetParent(transform, false);
+        Toast ts = t.GetComponent<Toast>();
+        ts.message = message;
+        ts.duration = duration;
+        Debug.Log("Toast(" + duration + "): " + message);
+    }
+}
---
+++ Assets/Scripts/Toast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toast : MonoBehaviour
+{
+    public string message;
+
+    public float duration;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).GetComponent<Text>().text = message;
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= time + duration) Destroy(this);
+    }
+
+    public static void Make(string message, float duration = 0.5f) {
+        GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
+        canvas.GetComponent<Canvas>().MakeToast(message, duration);
+    }
+}
