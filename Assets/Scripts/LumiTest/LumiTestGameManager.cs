48b564957d7aaefafdd60306c5b6065fe4a946a0
lumiknit <aasr4r4@gmail.com>
2019-01-21 00:43:08 +0900

    .

--- Assets/Scripts/LumiTest/LumiTestGameManager.cs
+++ Assets/Scripts/LumiTest/LumiTestGameManager.cs
@@ -7,11 +7,43 @@ public class LumiTestGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            var oz = GameObject.Find("human2a").GetComponent<OzHuman>();
+            oz.action = new OzHuman.TestHSwing1(oz);
+        }
+        if(Input.GetKeyDown(KeyCode.D)) {
+            var oz = GameObject.Find("human2a").GetComponent<OzHuman>();
+            oz.action = new OzHuman.TestRow1(oz);
+        }
+        if(Input.GetKeyDown(KeyCode.W)) {
+            var oz = GameObject.Find("human2a").GetComponent<OzHuman>();
+            oz.action = new OzHuman.TestRow3(oz);
+        }
+        if(Input.GetKeyDown(KeyCode.A)) {
+            var oz = GameObject.Find("human2a").GetComponent<OzHuman>();
+            oz.action = new OzHuman.TestRow2(oz);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            var oz = GameObject.Find("human2a").GetComponent<OzHuman>();
+            oz.action = new OzHuman.TestAction(oz);
+        }
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            GameObject.Find("human2a").GetComponent<OzHuman>().SetBoat(
+                GameObject.Find("bigflake1")
+            );
+        }
+        { /* Camera */
+            var camera = Camera.main;
+            var oz = GameObject.Find("spine1");
+            var p = oz.transform.position + oz.transform.rotation * new Vector3(0f, 0f, -10f) + new Vector3(0f, 2f, 0f);
+            camera.transform.position = p;
+            camera.transform.rotation = Quaternion.LookRotation(oz.transform.position - p, Vector3.up);
+        }
     }
+
 }
--- Assets/Scripts/Oz/OzHuman.cs
+++ Assets/Scripts/Oz/OzHuman.cs
@@ -15,41 +15,71 @@ public class OzHuman : MonoBehaviour
     GameObject shoulderL, arm1L, arm2L, handL;
     GameObject shoulderR, arm1R, arm2R, handR;
     GameObject head;
-    GameObject oar;
+    GameObject oar, oarEnd;
     GameObject boat;
+    GameObject targetSphere;
+    GameObject rotationSphere;
 
     /* Lassitude */
     bool lassitude = false;
 
     /* Target Angle (ya = latitude, xa = longitude) */
-    float xa = 0f, ya = 0f;
-    public float txa = 0f, tya = 0f;
+    float xa = 0f, ya = 0f, ra = 1.5f;
+    public float txa = 0f, tya = 0f, tra = 1.5f;
+    public float armSpd = 10f;
+    public Vector3 lOff = new Vector3(0, 0, 0);
     Vector3 targetPos;
 
     /* Reserved Action */
-    Action action;
+    public Action action;
+
     
     
     // Start is called before the first frame update
     void Start()
     {
         InitializeBodyObjects();
-        action = new TestHSwing1(this).Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(action != null && !action.RunStep()) {
+        if(action == null) {
+            txa = 20f;
+            tya = 0f;
+            tra = 3f;
+            lOff = Vector3.zero;
+            armSpd = 10f;
+        } else if(!action.RunStep()) {
             action = null;
         }
         if(!lassitude) {
             UpdateTargetPos();
-            UpdateArmPos(handL, shoulderL, new Vector3(0, 0, 0));
+            UpdateArmPos(handL, shoulderL, lOff);
             UpdateArmPos(handR, shoulderR, new Vector3(0, 0, 0));
             UpdateOarPos();
+            targetSphere.transform.position = targetPos;
+        }
+        if(boat != null) {
+            var ex = oarEnd.transform.position - transform.position;
+            var py = 0.2f;
+            if(ex.y < py) {
+                var dx = ex - oarEndLastPos;
+                dx.y = 0;
+                var dl = dx.magnitude;
+                var rBoat = boat.GetComponent<Rigidbody>();
+                var point = rBoat.worldCenterOfMass + rBoat.rotation * new Vector3(0.1f, 0f, 0f);
+                boat.GetComponent<Rigidbody>().AddForceAtPosition(-dx.normalized * 5 * dl * dl * Mathf.Abs(ex.y - py), point, ForceMode.VelocityChange);
+                Debug.Log("aa");
+                boat.GetComponent<Rigidbody>().velocity *= 0.98f;
+            }
+            oarEndLastPos = ex;
+            footL.GetComponent<Rigidbody>().velocity += new Vector3(0f, -2f, 0f);
+            footR.GetComponent<Rigidbody>().velocity += new Vector3(0f, -2f, 0f);
         }
     }
+
+    Vector3 oarEndLastPos;
 
     public void CascadeImpulse(Vector3 impulse, Vector3 point) {
         if(impulse.magnitude > LIMIT_IMPULSE) {
@@ -72,10 +102,10 @@ public class OzHuman : MonoBehaviour
         var s = shoulder.transform.position;
         var h = hand.transform.position;
         var a = (h - s).normalized;
-        var r = (targetPos + targetOffset - s).normalized;
+        var r = (targetPos + transform.rotation * targetOffset - s).normalized;
         var d = (Vector3.Cross(Vector3.Cross(a, r), a)).normalized;
-        var m = Mathf.Acos(Vector3.Dot(a, r)) * 10f;
-        rHand.velocity = d * m + a * 2f;
+        var m = Mathf.Acos(Vector3.Dot(a, r)) * armSpd;
+        rHand.velocity = GetComponent<Rigidbody>().velocity + d * m + a * 2f;
     }
 
     private void UpdateOarPos() {
@@ -85,17 +115,14 @@ public class OzHuman : MonoBehaviour
     }
 
     private void UpdateTargetPos() {
-        if(txa < -120f) txa = -120f;
-        else if(txa > 120f) txa = 120f;
-        if(tya < -85f) tya = -85f;
-        else if(ya > 85f) tya = 85f;
         xa = (xa + txa) / 2;
         ya = (ya + tya) / 2;
+        ra = (ra + tra) / 2;
         var sx = Mathf.Sin(Mathf.Deg2Rad * xa);
         var cx = Mathf.Cos(Mathf.Deg2Rad * xa);
         var sy = Mathf.Sin(Mathf.Deg2Rad * ya);
         var cy = Mathf.Cos(Mathf.Deg2Rad * ya);
-        targetPos = spine2.transform.position + new Vector3(cy * sx, sy, cy * cx) * 1.5f;
+        targetPos = spine3.transform.position + transform.rotation * new Vector3(cy * sx, sy, cy * cx) * ra;
     }
 
 
@@ -120,12 +147,24 @@ public class OzHuman : MonoBehaviour
         arm2R = arm1R.transform.Find("arm2_r").gameObject;
         handR = arm2R.transform.Find("hand_r").gameObject;
         head = spine3.transform.Find("head").gameObject;
+        targetSphere = top.transform.Find("TargetSphere").gameObject;
         oar = top.Find("oar").gameObject;
+        oarEnd = oar.transform.Find("Endpiece").gameObject;
         boat = null;
     }
 
+    public void SetBoat(GameObject boat) {
+        this.boat = boat;
+        FixedJoint joint;
+        if((joint = spine1.GetComponent<FixedJoint>()) != null) {
+            Destroy(joint);
+        }
+        joint = spine1.AddComponent<FixedJoint>();
+        joint.connectedBody = boat.GetComponent<Rigidbody>();
+    }
 
-    abstract class Action {
+
+    public abstract class Action {
         public OzHuman oz;
         public float beginningTime;
         public float lastTime;
@@ -133,12 +172,8 @@ public class OzHuman : MonoBehaviour
         public Action(OzHuman oz, float duration) {
             this.oz = oz;
             this.duration = duration;
-        }
-        public Action Start() {
             beginningTime = Time.time;
             lastTime = beginningTime;
-            RunStep();
-            return this;
         }
         public bool RunStep() {
             Step(Time.time - beginningTime, Time.time - lastTime);
@@ -149,30 +184,197 @@ public class OzHuman : MonoBehaviour
             }
             return true;
         }
-        public virtual void OnStart() {}
         public virtual void Step(float time, float deltaTime) {}
         public virtual void OnFinish() {}
         public bool IsFinished() {
             return Time.time - beginningTime >= duration;
         }
+
+        public float Progress(float t) { return (t - Time.time) / duration;}
+        public float TRange(float prog, float from, float to) {
+            if(from <= prog && prog < to) {
+                return (prog - from) / (to - from);
+            }
+            return float.NaN;
+        }
     }
 
-    class TestHSwing1 : Action {
-        public const float DURATION = 1f;
+    public class TestHSwing1 : Action {
+        public const float DURATION = 2f;
         public TestHSwing1(OzHuman oz) : base(oz, DURATION) {}
-        public override void OnStart() {
-            oz.txa = -90f;
-            oz.tya = 0f;
-        }
         public override void Step(float t, float dt) {
             float prog = t / DURATION;
-            if(prog < 0.5f) {
-                oz.txa = -90f;
+            float x;
+            if(!float.IsNaN(x = TRange(prog, 0.0f, 0.2f))) {
+                oz.txa = 120f;
+                oz.tya = -10f;
+                oz.tra = 1f;
+                oz.lOff = new Vector3(0, 0, 0);
+                oz.armSpd = 20f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.2f, 0.3f))) {
+                oz.txa = 0f;
+                oz.tya = -10f;
+                oz.tra = 1f;
+                oz.lOff = new Vector3(0.8f, 0.0f, 0.3f);
+                oz.armSpd = 20f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.3f, 0.5f))) {
+                oz.txa = -120f;
+                oz.tya = -10f;
+                oz.tra = 1f;
+                oz.lOff = new Vector3(1.1f, 0.0f, 0.3f);
+                oz.armSpd = 20f;
             } else {
-                oz.txa = - (prog - 0.5f) / 0.5f * 180 + 90f;
+                oz.txa = 0f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+                oz.lOff = new Vector3(0, 0, 0);
             }
-            
-            Debug.Log("aa");
+        }
+    }
+
+    public class TestHSwing2 : Action {
+        public const float DURATION = 1f;
+        public TestHSwing2(OzHuman oz) : base(oz, DURATION) {}
+        public override void Step(float t, float dt) {
+            float prog = t / DURATION;
+            float x;
+            if(!float.IsNaN(x = TRange(prog, 0.0f, 0.06f))) {
+                oz.txa = 120f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.06f, 0.12f))) {
+                oz.txa = 0f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.12f, 0.20f))) {
+                oz.txa = -60f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.20f, 0.3f))) {
+                oz.txa = -90f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.3f, 0.4f))) {
+                oz.txa = -120f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+            } else {
+                oz.txa = 0f;
+                oz.tya = 0f;
+                oz.tra = 1f;
+            }
+        }
+    }
+
+    public class TestRow1 : Action {
+        public const float DURATION = 1.8f;
+        public TestRow1(OzHuman oz) : base(oz, DURATION) {}
+        public override void Step(float t, float dt) {
+            float prog = t / DURATION;
+            float x;
+            if(!float.IsNaN(x = TRange(prog, 0.0f, 0.125f))) {
+                oz.txa = -40f;
+                oz.tya = 0f;
+                oz.tra = 2f;
+                oz.lOff = new Vector3(-0.4f, 0.5f, 0);
+                oz.armSpd = 40f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.125f, 0.25f))) {
+                oz.txa = -30f;
+                oz.tya = -10f;
+                oz.tra = 2f;
+                oz.lOff = new Vector3(0, 4f, 0);
+                oz.armSpd = 20f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.25f, 0.5f))) {
+                oz.txa = 60f;
+                oz.tya = -10f;
+                oz.tra = 2f;
+                oz.lOff = new Vector3(0, 2f, 0);
+                oz.armSpd = 30f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.5f, 1.0f))) {
+                oz.txa = 30f;
+                oz.tya = 10f;
+                oz.tra = 1f;
+                oz.lOff = new Vector3(0, 0f, 0);
+                oz.armSpd = 15f;
+            }
+        }
+    }
+
+    public class TestRow3 : Action {
+        public const float DURATION = 1.8f;
+        public TestRow3(OzHuman oz) : base(oz, DURATION) {}
+        public override void Step(float t, float dt) {
+            float prog = t / DURATION;
+            float x;
+            if(!float.IsNaN(x = TRange(prog, 0.0f, 0.125f))) {
+                oz.txa = -20f;
+                oz.tya = 0f;
+                oz.tra = 2f;
+                oz.lOff = new Vector3(-0.4f, 0.5f, 0);
+                oz.armSpd = 40f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.125f, 0.25f))) {
+                oz.txa = -20f;
+                oz.tya = -10f;
+                oz.tra = 2f;
+                oz.lOff = new Vector3(0, 4f, 0);
+                oz.armSpd = 20f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.25f, 0.5f))) {
+                oz.txa = 110f;
+                oz.tya = -10f;
+                oz.tra = 2f;
+                oz.lOff = new Vector3(0.8f, 3f, 0);
+                oz.armSpd = 30f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.5f, 1.0f))) {
+                oz.txa = 30f;
+                oz.tya = 10f;
+                oz.tra = 1f;
+                oz.lOff = new Vector3(0, 0f, 0);
+                oz.armSpd = 15f;
+            }
+        }
+    }
+
+    public class TestRow2 : Action {
+        public const float DURATION = 1.5f;
+        public TestRow2(OzHuman oz) : base(oz, DURATION) {}
+        public override void Step(float t, float dt) {
+            float prog = t / DURATION;
+            float x;
+            if(!float.IsNaN(x = TRange(prog, 0.0f, 0.4f))) {
+                oz.txa = 80f;
+                oz.tya = -30f;
+                oz.tra = 3.5f;
+                oz.lOff = new Vector3(-1.8f, 2.1f, -0.0f);
+                oz.armSpd = 20f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.4f, 0.5f))) {
+                oz.txa = -30f;
+                oz.tya = -20f;
+                oz.tra = 4f;
+                oz.lOff = new Vector3(-0.8f, 2.3f, -1.6f);
+                oz.armSpd = 50f;
+            } else if(!float.IsNaN(x = TRange(prog, 0.5f, 1f))) {
+                oz.txa = -30f;
+                oz.tya = -20f;
+                oz.tra = 4f;
+                oz.lOff = new Vector3(-0.8f, 1f, -1.6f);
+                oz.armSpd = 50f;
+            }
+        }
+    }
+
+    public class TestAction : Action {
+        public const float DURATION = 2f;
+        public TestAction(OzHuman oz) : base(oz, DURATION) {}
+        public override void Step(float t, float dt) {
+            float prog = t / DURATION;
+            float x;
+            if(!float.IsNaN(x = TRange(prog, 0.0f, 1f))) {
+                oz.txa = -20f;
+                oz.tya = 0f;
+                oz.tra = 1.1f;
+                oz.lOff = new Vector3(0.5f, 0f, -1f);
+                oz.armSpd = 40f;
+            }
         }
     }
 }
